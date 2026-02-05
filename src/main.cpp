dca1d5be9dae2518bcbb3222e973014114e22b88
Feivel <bene.fei.k@gmail.com>
2026-02-05 23:16:11 +0700

    modified alert logic and features

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
-#include "wifi_manager.h"
+#include "wifi_server.h"
 
 #define DEBUG
 
 const int LED_pins = 26;
 const int Sensor_pin = 32;
 
-const int QualityLimit = 1500;
+const int QualityLimit = 1200;
 const int deviationLimit = 250;
 
 DeviceServer wifi(80);
+
+int airQuality = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -20,7 +22,7 @@ void setup() {
   Serial.println("running...");
   #endif
   
-  (wifi.begin(Sensor_pin))? Serial.println("WiFi started successfully!") : Serial.println("Failed to start WiFi!");
+  (wifi.begin())? Serial.println("WiFi started successfully!") : Serial.println("Failed to start WiFi!");
 
   pinMode(LED_pins, OUTPUT);
   pinMode(Sensor_pin, INPUT);
@@ -32,9 +34,14 @@ void loop() {
   
   int sensorValue = analogRead(Sensor_pin);
   int delta = abs(sensorValue - prev_sensorValue);
-  prev_sensorValue = sensorValue;
   
-  bool airQuality = (sensorValue < QualityLimit && delta < deviationLimit);
+  airQuality = map(analogRead(Sensor_pin), 100, 2500, 50, 2000);
+  airQuality = constrain(airQuality, 50, 2000);
+  
+  bool Alert = (prev_sensorValue != 0 && delta > deviationLimit);
+  bool Danger = (airQuality > QualityLimit);
+  
+  prev_sensorValue = sensorValue;
   
   #ifdef DEBUG
   Serial.println("-----------------------");
@@ -42,13 +49,13 @@ void loop() {
   Serial.println(sensorValue);
   Serial.print("Delta: ");
   Serial.println(delta);
-  Serial.print("Air Quality: ");
-  (airQuality)? Serial.println("Good") : Serial.println("Poor");
+  (Alert || Danger)? Serial.println("LED ON") : Serial.println("LED OFF");
   Serial.println("-----------------------\n");
   #endif
   
-  if (airQuality) digitalWrite(LED_pins, LOW);
+  if (!Alert && !Danger) digitalWrite(LED_pins, LOW);
   else digitalWrite(LED_pins, HIGH);
+
   
   delay(1000);
 }
